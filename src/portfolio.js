06e39f2cdf30c1d4c53330187d5a2d47de18fb36
Jefrey Zavala <jefreyzavala1@gmail.com>
2023-09-15 12:01:17 -0400

    portfolio edits

--- src/portfolio.js
+++ src/portfolio.js
@@ -20,8 +20,15 @@ const about = {
 
 const projects = [
   {
+    name: "Fallmart",
+    description: "A full stack e-commerce website built in React.",
+    stack: ["MongoDB", "Express", "React", "Node.js"],
+    sourceCode: "https://github.com/jefreyzavala1/Walmart-MERN-APP",
+    livePreview: "https://fallmart.josemonagas.me/",
+  },
+  {
     name: "Kickquest",
-    description: "Full stack shoe selling website built in react.",
+    description: "A full stack shoe selling website built in react.",
     stack: ["MongoDb", "Express", "React", "Node.js"],
     sourceCode: "https://github.com/jefreyzavala1/unit-3-project",
     livePreview: "https://kickquest.jefreyzavala.me/",
@@ -47,10 +54,10 @@ const projects = [
   {
     name: "Simply Blogging",
     description:
-      "Full stack app that empowers users to create blogs and connect with others",
-    stack: ["JavaScript", "Boostrap", "Node.js", "Express", "MongoDB", "Ejs"],
+      "Full stack app that empowers users to create blogs and connect with others.",
+    stack: ["JavaScript", "Bootstrap", "Node.js", "Express", "MongoDB", "Ejs"],
     sourceCode: "https://github.com/jefreyzavala1/simply-blogging",
-    livePreview: "https://i.imgur.com/yLJCcry.jpg",
+    livePreview: "https://simply-blogging.herokuapp.com/",
     // img: "https://i.imgur.com/yLJCcry.jpg",
   },
 
@@ -58,7 +65,7 @@ const projects = [
     name: "Appointy",
     description:
       "Full Stack app that enables users to manage their appointments.",
-    stack: ["JavaScript", "Boostrap", "Node.js", "Express", "MongoDB", "Ejs"],
+    stack: ["JavaScript", "Bootstrap", "Node.js", "Express", "MongoDB", "Ejs"],
     sourceCode: "https://github.com/jefreyzavala1/appointment-mvc-auth",
     livePreview: "https://appointy-portal.herokuapp.com/",
     // img: "https://i.imgur.com/xnF7NGU.jpg",
@@ -66,8 +73,8 @@ const projects = [
   {
     name: "Course Platform",
     description:
-      "Course management tool for adding removing and updating course information",
-    stack: ["JavaScript", "Boostrap", "Node.js", "Express", "MongoDB", "Ejs"],
+      "Course management tool for adding removing and updating course information.",
+    stack: ["JavaScript", "Bootstrap", "Node.js", "Express", "MongoDB", "Ejs"],
     sourceCode: "https://github.com/jefreyzavala1/school-course-app",
     livePreview: "https://class-courses-app.herokuapp.com/",
     // img: "https://i.imgur.com/nspjbHZ.jpg",
@@ -76,15 +83,16 @@ const projects = [
   {
     name: "Puzzle Game",
     description: "Can you rearange the picture back to its orginal?",
-    stack: ["HTML", "CSS", "JavaScript"],
+    stack: ["Html", "Css", "JavaScript"],
     sourceCode: "https://github.com/jefreyzavala1/puzzlegame",
     livePreview: "https://jefreypuzzlegame.herokuapp.com/",
     // img: "https://i.imgur.com/gBfa4PL.jpg",
   },
   {
     name: "NewsLetter App",
-    description: "Using the Mailchimp API to subscribe users to a mailing list",
-    stack: ["Html", "Css", "Bootstrap", "Node.js", "Express"],
+    description:
+      "Using the Mailchimp API to subscribe users to a mailing list.",
+    stack: ["Html", "Css", "Bootstrap", "Node.js", "Express", "JavaScript"],
     sourceCode: "https://github.com/jefreyzavala1/mailchimp-newsletter-api",
     livePreview: "https://safe-wildwood-38967.herokuapp.com/",
     // img: "https://i.imgur.com/QU1nhuO.jpg",
@@ -92,8 +100,16 @@ const projects = [
   {
     name: "Task Tracker",
     description:
-      "A full stack web application created to help users in efficiently managing their daily tasks and to-do",
-    stack: ["Html", "Css", "Bootstrap", "Node.js", "Express", "MongoDB"],
+      "A full stack web application created to help users in efficiently managing their daily tasks and to-do.",
+    stack: [
+      "Html",
+      "Css",
+      "Bootstrap",
+      "Node.js",
+      "Express",
+      "MongoDB",
+      "JavaScript",
+    ],
     sourceCode: "https://github.com/jefreyzavala1/task-tracker",
     livePreview: "https://task-tracker-list-app.herokuapp.com/",
     // img: "https://i.imgur.com/UJeoZvg.jpg",
